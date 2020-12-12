2cfc6c1bfcd7f48d6e962db5891b1ade2ab91732
chintensakai <toy_shen@163.com>
2020-12-13 00:51:20 +0800

    chins: 初始化项目结构

---
+++ chinsmall-order/src/test/java/com/chins/chinsmall/order/ChinsmallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chins.chinsmall.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChinsmallOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
