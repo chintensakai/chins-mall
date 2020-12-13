1c273cf8ccd615960b150fc6c6c24db644a770e9
chintensakai <toy_shen@163.com>
2020-12-13 14:05:54 +0800

    chins: 添加mybatis-plus

--- README.md
+++ README.md
@@ -3,4 +3,11 @@
 ```
 sudo docker run -d --name mysql -v /home/ubuntu/volumes/mysql:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=123456 -p 3316:3306 mysql
 ```
+| 服务    | 端口 |
+| ------- | ---- |
+| member  | 5000 |
+| order   | 6000 |
+| product | 7000 |
+| coupon  | 8000 |
+| ware    | 9000 |
 
--- chinsmall-product/src/main/java/com/chins/chinsmall/product/controller/PmsBrandController.java
+++ chinsmall-product/src/main/java/com/chins/chinsmall/product/controller/PmsBrandController.java
@@ -5,7 +5,6 @@ import com.chins.chinsmall.product.service.IPmsBrandService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import org.springframework.web.bind.annotation.RestController;
 
 /**
