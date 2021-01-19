20073436a9b86ae3870aff51dddcd2dd335143a8
chintensakai <toy_shen@163.com>
2021-01-19 23:27:43 +0800

    chins: 配置网关跨域过滤器

---
+++ chinsmall-gateway/src/main/java/com/chins/chinsmall/gateway/config/CoreConfig.java
@@ -0,0 +1,25 @@
+package com.chins.chinsmall.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CoreConfig {
+
+  @Bean
+  public CorsWebFilter gatewayCorsWebFilter() {
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    CorsConfiguration corsConfiguration = new CorsConfiguration();
+    corsConfiguration.addAllowedHeader("*");
+    corsConfiguration.addAllowedMethod("*");
+    corsConfiguration.addAllowedOrigin("*");
+    corsConfiguration.setAllowCredentials(true);
+
+    source.registerCorsConfiguration("/**", corsConfiguration);
+
+    return new CorsWebFilter(source);
+  }
+}
